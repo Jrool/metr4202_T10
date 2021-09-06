10acb669c1737f9e09bc7225011652f4fef34731
Jrool <josh201181@gmail.com>
2021-09-07 00:25:34 +1000

    Got the stubby arm working, this arm features a workspace like the one we will be using on demo day

--- rosstuff/catkin_ws/devel/_setup_util.py
+++ rosstuff/catkin_ws/devel/_setup_util.py
@@ -268,7 +268,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = r'/opt/ros/noetic'.split(';')
+            CMAKE_PREFIX_PATH = r'/home/fred-ross/Documents/METR4202/Repo/rosstuff/catkin_ws/devel;/opt/ros/noetic'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
