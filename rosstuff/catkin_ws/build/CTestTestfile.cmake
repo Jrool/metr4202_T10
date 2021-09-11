0df7286d7175edef7cdd2ab2e49d3dc4985405bb
Jrool <josh201181@gmail.com>
2021-09-11 18:26:42 +1000

    Added Michael's changes to own branch

--- rosstuff/catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fred-ross/Documents/METR4202/Repo/rosstuff/catkin_ws/src
-# Build directory: /home/fred-ross/Documents/METR4202/Repo/rosstuff/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("robot_simulation")
